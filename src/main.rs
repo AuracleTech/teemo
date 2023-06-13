28a5b8cbf9aaf05c9614ad28080cbca573db5bc8
AuracleTech <VirgilFabre@outlook.com>
2023-06-12 23:59:39 -0400

    Dynamic memory allocator

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use teemo::println;
 
+extern crate alloc;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use teemo::allocator;
     use teemo::memory;
     use teemo::memory::BootInfoFrameAllocator;
     use x86_64::{structures::paging::Page, VirtAddr};
@@ -30,6 +34,32 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // write the string `New!` to the screen through the new mapping
     let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     // as before
     #[cfg(test)]
