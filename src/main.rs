822442df28da19d69ebb42ea2495c3c27e10fbea
AuracleTech <VirgilFabre@outlook.com>
2023-06-15 01:02:28 -0400

    Remove warnings

--- src/main.rs
+++ src/main.rs
@@ -11,41 +11,25 @@ use teemo::task::executor::Executor;
 use teemo::task::keyboard;
 
 extern crate alloc;
-use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 
 entry_point!(kernel_main);
-
-async fn async_number() -> u32 {
-    // huge calcul to slow down the program
-
-    for i in 0..10000000 {
-        let _ = i;
-    }
-
-    42
-}
-
-async fn example_task() {
-    let number = async_number().await;
-    println!("async number: {}", number);
-}
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use teemo::allocator;
     use teemo::memory;
     use teemo::memory::BootInfoFrameAllocator;
     use teemo::task::Task;
-    use x86_64::{structures::paging::Page, VirtAddr};
+    use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
     teemo::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    // let mut frame_allocator = memory::EmptyFrameAllocator;
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     // // map an unused page
+    // use x86_64::structures::paging::Page;
     // let page = Page::containing_address(VirtAddr::new(0));
     // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
@@ -55,35 +39,10 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // allocate a number on the heap
-    // let heap_value = Box::new(41);
-    // println!("heap_value at {:p}", heap_value);
-
-    // // create a dynamically sized vector
-    // let mut vec = Vec::new();
-    // for i in 0..500 {
-    //     vec.push(i);
-    // }
-    // println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    // let reference_counted = Rc::new(vec![1, 2, 3]);
-    // let cloned_reference = reference_counted.clone();
-    // println!(
-    //     "current reference count is {}",
-    //     Rc::strong_count(&cloned_reference)
-    // );
-    // core::mem::drop(reference_counted);
-    // println!(
-    //     "reference count is {} now",
-    //     Rc::strong_count(&cloned_reference)
-    // );
-
     #[cfg(test)]
     test_main();
 
     let mut executor = Executor::new();
-    executor.spawn(Task::new(example_task()));
     executor.spawn(Task::new(keyboard::print_keypresses()));
     executor.run();
 }
